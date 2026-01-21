c2d84c6ba9e79b9bd25a72b20c1ab9c00186f531
Ryan <ryan.rogowski@mymail.champlain.edu>
2026-01-21 15:11:45 -0500

    Process Managment 1

---
+++ week2/ProcessManagement1_Chrome.ps1
@@ -0,0 +1,6 @@
+﻿
+if (Get-Process -Name chrome) {
+    Get-Process -Name chrome | kill
+} else {
+    Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" "--new-window www.champlain.edu"
+}
