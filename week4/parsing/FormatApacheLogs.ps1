2d0c421686a947265ebfacc685b5d93f75f594d7
Ryan <ryan.rogowski@mymail.champlain.edu>
2026-02-04 15:44:07 -0500

    Parsing Apache Logs

---
+++ week4/parsing/FormatApacheLogs.ps1
@@ -0,0 +1,26 @@
+﻿function getApacheLogs() {
+$lines = Get-Content "C:\xampp\apache\logs\access.log"
+
+$logTable = @()
+
+for ($i=0; $i -lt $lines.Length; $i++) {
+    $data = $lines[$i].Split(" ")
+    
+    
+    $logTable += [pscustomobject]@{"IP" = $data[0]; `
+                                   #"Client" = $data[1]; `
+                                   #"Requestor" = $data[2]; `
+                                   "Time" = $data[3]; `
+                                   #"Timezone" = $data[4]; `
+                                   "Method" = $data[5]; `
+                                   "Resource" = $data[6]; `
+                                   "Protocol" = $data[7]; `
+                                   "Code" = $data[8]; `
+                                   "Size" = $data[9]; `
+                                   "Referrer" = $data[10]; `
+                                   "UserAgent" = $data[11..($data.Count -1)];
+                                  }
+}
+
+return $logTable
+}
---
+++ week4/parsing/main.ps1
@@ -0,0 +1,8 @@
+﻿. (Join-Path $PSScriptRoot FormatApacheLogs.ps1)
+
+$logs = getApacheLogs
+
+Write-Host ($logs | Format-Table -AutoSize | Out-String)
+
+Write-Host ($logs | Where-Object { $_.Resource -ilike "*page2.html" -and $_.Referrer -ilike "*index.html*"} `
+    | Format-Table | Out-String)
