8c7bff4bcb0704850099762260ec798c67acc0d0
Ryan <ryan.rogowski@mymail.champlain.edu>
2026-02-04 13:27:10 -0500

    Windows Apache Logs

---
+++ week4/apacheLogs.ps1
@@ -0,0 +1,17 @@
+﻿#Get-Content C:\xampp\apache\logs\access.log | Select-String -Last 5
+#Get-Content C:\xampp\apache\logs\access.log | Select-String " 400[0-9] "
+#Get-Content C:\xampp\apache\logs\access.log | Select-String " 200 " -NotMatch
+
+# Q5
+$logspath = "C:\xampp\apache\logs\"
+Get-ChildItem $logspath | Select-String "error"
+
+clear
+# Q6/7
+$logs = Get-Content "C:\xampp\apache\logs\access.log"
+
+$lines = @()
+for ($i=0; $i -lt $logs.Count; $i++) {
+    $lines += $logs[$i].Split(" - - \[")[0]
+}
+$lines | Select-String ":" -NotMatch | Group-Object
