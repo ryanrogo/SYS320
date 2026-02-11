0e6a0f13bf4b7d1851905a6b93e0ba79b7429172
Ryan <ryan.rogowski@mymail.champlain.edu>
2026-02-11 12:56:48 -0500

    Web scraping - class work

---
+++ week5/HTML_elements_webRequests.ps1
@@ -0,0 +1,13 @@
+﻿$scaped_page = Invoke-WebRequest -TimeoutSec 10 http://localhost/ToBeScraped.html
+#$scaped_page.links.Count
+
+#$scaped_page.links | select href, outerText
+
+$h2s = $scaped_page.ParsedHtml.body.getElementsByTagName("h2") | select outerText
+#$h2s
+
+
+$div1s = $scaped_page.ParsedHtml.body.getElementsByTagName("div") | where { `
+    $_.getAttributeNode("class").Value -ilike "div-1" } | select innerText
+
+$div1s
