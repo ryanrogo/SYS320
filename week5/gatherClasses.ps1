85a11dff3f90e8bb7c86424c6fd6977aa760c010
Ryan <ryan.rogowski@mymail.champlain.edu>
2026-02-11 21:17:16 -0500

    Scraping Champlain Classes

---
+++ week5/gatherClasses.ps1
@@ -0,0 +1,25 @@
+﻿function gatherClasses() {
+    
+    $page = Invoke-WebRequest -TimeoutSec 2 http://localhost/Courses2026SP.html
+    $trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+    
+    $FullTable = @()
+
+    for ($i=1; $i -lt $trs.length; $i++) {
+        $tds = $trs[$i].getElementsByTagName("td")
+  
+        $Times = $tds[5].innerText.split("-")
+
+        $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText; `
+                                        "Title"      = $tds[1].innerText; `
+                                        "Days"       = $tds[4].innerText; `
+                                        "Time Start" = $Times[0]; `
+                                        "Time End"   = $Times[1]; `
+                                        "Instructor" = $tds[6].innerText; `
+                                        "Location"   = $tds[9].innerText; `
+                                        }
+                                        
+        
+    }
+return $FullTable
+}
---
+++ week5/main.ps1
@@ -0,0 +1,33 @@
+﻿. (Join-Path $PSScriptRoot gatherClasses.ps1)
+. (Join-Path $PSScriptRoot daysTranslator.ps1)
+
+$FullTable = gatherClasses
+
+$FullTable = daysTranslator $FullTable
+
+# all classes taugh by Prof Furkan Paligu
+$FullTable | Select-Object "Class Code", "Instructor", "Location", "Days", "Time Start", "Time End" | `
+    Where { $_."Instructor" -ilike "Furkan Paligu" }
+
+clear 
+# FREE 105 on Wednesday
+$FullTable | Where-Object { ($_.Location -ilike "FREE 105") -and ($_.days -ilike "Wednesday") } | `
+    Sort-Object "Time Start" -Descending | Format-Table "Time Start", "Time End", "Class Code"
+
+
+# Profs that teach SYS, NET, SEC, FOR, CSI, DAT
+$ITSinstructors = $FullTable | Where-Object {   ($_."Class Code" -ilike "SYS*") -or `
+                                                ($_."Class Code" -ilike "NET*") -or `
+                                                ($_."Class Code" -ilike "SEC*") -or `
+                                                ($_."Class Code" -ilike "FOR*") -or `
+                                                ($_."Class Code" -ilike "CSI*") -or `
+                                                ($_."Class Code" -ilike "DAT*") } `
+                             | Sort-Object "Instructor" `
+                             | Select-Object "Instructor" -Unique
+
+$instructors = $ITSinstructors.Instructor
+
+clear
+
+$FullTable | Where-Object {$_.Instructor -in $instructors} `
+| Group-Object "Instructor" | Select-Object Count,Name | Sort-Object Count -Descending
