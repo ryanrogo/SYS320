b4ba79a3beeac9caab27a2d609ec3e5b1d648672
Ryan <ryan.rogowski@mymail.champlain.edu>
2026-02-18 16:47:09 -0500

    Make Chrome script a function

--- week2/ProcessManagement1_Chrome.ps1
+++ week2/ProcessManagement1_Chrome.ps1
@@ -1,6 +1,7 @@
-﻿
-if (Get-Process -Name chrome) {
-    Get-Process -Name chrome | kill
-} else {
-    Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" "--new-window www.champlain.edu"
+﻿function visitChamplain() {
+    if (Get-Process -Name chrome) {
+        Get-Process -Name chrome | kill
+    } else {
+        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" "--new-window www.champlain.edu"
+    }
 }
